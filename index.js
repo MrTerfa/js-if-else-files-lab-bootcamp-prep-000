277b595eb343cb21ea4a8da39d4903d7be0c1355
Terfa Tortema <terfa.tortema@gmail.com>
2017-09-08 14:13:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 var name = "Susan"
 var height = "74"
-if (name === "Susan"){
-  console.log("The name is corect");
-}
-else{
-  console.log("Expected the name to be Susan");
-}
-
-
 // Don't worry about this
 module.exports = { name, height
 }
